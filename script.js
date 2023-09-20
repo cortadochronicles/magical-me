16331a2008ee08996092e6e36ddb0b9cee7c282c
cortadochronicles <143422448+cortadochronicles@users.noreply.github.com>
2023-09-20 14:25:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function countryAnswer(){
+  var password = document.getElementById("countryQuestion");
+  var passwordText = password.value;
+  if(passwordText=="80"){
+    return true;
+  }
+  alert("Not quite:/")
+}
